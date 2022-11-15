310a12aede49dcbfc774b0e9a84f615a408e79be
borges10002 <borges10002@gmail.com>
2022-11-15 10:59:32 -0400

    inserindo os dados do upload no respositorio

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,19 +1,57 @@
-import csvParse from "csv-parser";
+import { parse as csvParse } from "csv-parse";
 import fs from "fs";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 class ImportCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
 
-    const parseFile = csvParse();
+      const parseFile = csvParse();
 
-    stream.pipe(parseFile);
+      stream.pipe(parseFile);
 
-    parseFile.on("data", async (line) => {
-      console.log(line);
+      parseFile
+        .on("data", async (line) => {
+          //["name", "description"]
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+
+          console.log(categories);
+        })
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const existCategory = this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
     });
   }
 }
