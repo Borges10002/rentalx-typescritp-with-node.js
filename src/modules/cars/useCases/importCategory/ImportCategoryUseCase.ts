eb531249a776e9e8c429515274383a7fed652098
borges10002 <borges10002@gmail.com>
2022-11-15 07:29:04 -0400

    t

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,20 @@
+import csvParse from "csv-parser";
+import fs from "fs";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
 class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
