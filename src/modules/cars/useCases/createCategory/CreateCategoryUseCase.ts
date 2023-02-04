f0b210d735ba87d66cb9ffd5089094f29b9d866d
borges10002 <borges10002@gmail.com>
2023-02-03 22:41:24 -0400

    trabalhando com banco de dados

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -7,8 +7,11 @@ interface IRequest {
 
 class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
-  execute({ description, name }: IRequest): void {
-    const categoryAleadyExists = this.categoriesRepository.findByName(name);
+
+  async execute({ description, name }: IRequest): Promise<void> {
+    const categoryAleadyExists = await this.categoriesRepository.findByName(
+      name
+    );
 
     if (categoryAleadyExists) {
       throw new Error("Category Already exists!");
--- src/server.ts
+++ src/server.ts
@@ -15,4 +15,6 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(3333, () => console.log("Server is running"));
+const port = 3333;
+
+app.listen(port, () => console.log(`Server is running - ${port}`));
