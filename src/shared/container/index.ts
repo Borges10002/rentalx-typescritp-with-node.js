08d35fb9ba56e891759bd10bc0d6531c0a48fce5
borges10002 <borges10002@gmail.com>
2023-02-06 22:12:40 -0400

    criptografar senha

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,8 @@
 import { container } from "tsyringe";
 
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUserRepository } from "../../modules/accounts/repositories/IUserRepository";
+
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
 
@@ -14,4 +17,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+);
+
+container.registerSingleton<IUserRepository>(
+  "UsersRepository",
+  UsersRepository
 );
