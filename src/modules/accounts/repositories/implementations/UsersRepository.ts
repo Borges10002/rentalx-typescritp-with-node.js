e504bdecb61c2f8dabd973a16ffc29c79b63e66f
borges10002 <borges10002@gmail.com>
2023-02-19 20:11:37 -0400

    avatar de usuario

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -16,12 +16,16 @@ class UsersRepository implements IUserRepository {
     email,
     driver_license,
     password,
+    avatar,
+    id,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
       email,
       driver_license,
       password,
+      avatar,
+      id,
     });
 
     await this.repository.save(user);
--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
+
 import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 
 class UpdateUserAvatarController {
-  async handle(request: Request, response: Response): Promise<Request> {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.user;
 
-    const avatar_file = null;
+    const avatar_file = request.file.filename;
+
+    console.log(avatar_file);
 
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
     await updateUserAvatarUseCase.execute({ user_id: id, avatar_file });
 
-    response.status(204).send();
+    return response.status(204).send();
   }
 }
 
