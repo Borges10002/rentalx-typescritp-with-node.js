e3c6b4846d372ce535441d9d0379487579ba526a
Borges10002 <borges10002@gmail.com>
2023-03-22 21:03:45 -0300

    criado a tabela rentals

--- src/shared/infra/typeorm/migrations/1675561322369-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1675561322369-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1675561322369 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
