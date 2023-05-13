d0a3b855f786fed0eacd302359249afd83482442
Borges10002 <borges10002@gmail.com>
2023-05-13 16:44:05 -0400

    finally reset password

--- src/shared/container/providers/IMailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/IMailProvider/implementations/EtherealMailProvider.ts
@@ -1,6 +1,8 @@
 import { injectable } from "tsyringe";
 import { IMailProvider } from "../IMailProvider";
 import nodemailer, { Transporter } from "nodemailer";
+import handlebars from "handlebars";
+import fs from "fs";
 
 @injectable()
 class EtherealMailProvider implements IMailProvider {
@@ -24,12 +26,23 @@ class EtherealMailProvider implements IMailProvider {
       })
       .catch((err) => console.log(err));
   }
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(
+    to: string,
+    subject: string,
+    variable: any,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variable);
+
     const message = await this.client.sendMail({
       to,
       from: "Rentx <noreplary@rentx.com.br>",
-      text: body,
-      html: body,
+      subject,
+      html: templateHTML,
     });
 
     console.log("Message sent: %s", message.messageId);
